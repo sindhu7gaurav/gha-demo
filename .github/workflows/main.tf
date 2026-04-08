87e3f79641adb0420ff734afd08f0327fc4a44a5
sindhu7gaurav <sindhu7gaurav@gmail.com>
2026-04-08 22:14:23 +0530

    Create main.tf

---
+++ .github/workflows/main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project     = "YOUR_PROJECT_ID"
+  region      = "us-central1"
+  credentials = jsondecode(var.gcp_credentials)
+}
+
+variable "gcp_credentials" {
+  type = string
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
